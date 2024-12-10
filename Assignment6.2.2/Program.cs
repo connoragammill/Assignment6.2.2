52826a4ee11b7ded352899ed991051cac6d916e6
connor gammill <connor.gammill@outlook.com>
2024-12-10 10:48:13 -0500

    Add project files.

---
+++ Assignment6.2.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace Assignment6._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [1, 2, 3, 4];
+
+			int[] answer = new int[nums.Length];
+			int product = 0;
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				product = 1;
+				for (int j = 0; j < nums.Length; j++)
+				{
+					if (j != i)
+					{
+						product *= nums[j];
+					}
+				}
+				answer[i] = product;
+			}
+			foreach (int num in answer)
+			{
+				Console.Write(num + " ");
+			}
+		}
+    }
+}
